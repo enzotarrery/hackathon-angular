818fc8071cb64bb9080fdc62ddf1c91b643fcf42
fgroisil <flavien.groisillier@etudiant.univ-lr.fr>
2022-03-31 12:13:14 +0200

    init server avec test data

---
+++ server/src/api/stuff-type/routes/stuff-type.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * stuff-type router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::stuff-type.stuff-type');
