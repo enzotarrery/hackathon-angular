232aa740170d3aa2185d0c8f395d20d70d4e0bee
Antoine Marionneau <antoine.marionneau@etudiant.univ-lr.fr>
2022-04-01 08:34:06 +0200

    api prefix

--- client/src/app/core/services/trip.service.ts
+++ client/src/app/core/services/trip.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { STRAPI_API } from '../globals';
 
 @Injectable({
   providedIn: 'root'
@@ -14,7 +15,7 @@ export class TripService {
       'Content-Type': 'application/json',
       'Authorization': `Bearer eb6bc3bfea7804faa5640c491ebbdc6c43fe2d6647aeac94f37315a27162fd547097243b4064fd1546cfeae8cb67a2ad19492a18341368cb0a3056021e33ffaede290c6d5e9f4c8e0ddfcb54e6e78dc742d3275e7dea5dc6ce8d9970b732a41bddce94eb5d2877fe52571f681cdff634bdcb32a1542f30b52112734a84df42a5`,
     };
-    return this.httpClient.get(`http://localhost:1337/api/trips?populate=%2A`, { headers: opts });
+    return this.httpClient.get(STRAPI_API + `trips?populate=%2A`, { headers: opts });
   }
 
 
