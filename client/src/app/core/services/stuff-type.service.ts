063ac060b8f85cd6d1bbeff5af5d0681beb061cd
Enzo Tarrery <enzo.tarrery@etudiant.univ-lr.fr>
2022-04-01 09:52:15 +0200

    Fix: style & variables

--- client/src/app/core/services/stuff-type.service.ts
+++ client/src/app/core/services/stuff-type.service.ts
@@ -1,24 +1,31 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { TokenStorageService } from './token-storage.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class StuffTypeService {
-
-  constructor(private httpClient: HttpClient) { }
-  
+  constructor(
+    private tokenStorageService: TokenStorageService,
+    private httpClient: HttpClient
+  ) {}
 
   getStuffTypes(): Observable<any> {
     const opts = {
       'Content-Type': 'application/json',
-      'Authorization': `Bearer eb6bc3bfea7804faa5640c491ebbdc6c43fe2d6647aeac94f37315a27162fd547097243b4064fd1546cfeae8cb67a2ad19492a18341368cb0a3056021e33ffaede290c6d5e9f4c8e0ddfcb54e6e78dc742d3275e7dea5dc6ce8d9970b732a41bddce94eb5d2877fe52571f681cdff634bdcb32a1542f30b52112734a84df42a5`,
+      Authorization: `Bearer ${this.tokenStorageService.getToken()}`,
     };
-    return this.httpClient.get(`http://localhost:1337/api/stuff-types?populate=%2A`, { headers: opts });
+    return this.httpClient.get(
+      `http://localhost:1337/api/stuff-types?populate=%2A`,
+      { headers: opts }
+    );
   }
 
   searchStuff(param: string): Observable<any> {
-    return this.httpClient.get('http://localhost:1337/api/stuff-types?filters[name][$contains]=' + param);
+    return this.httpClient.get(
+      'http://localhost:1337/api/stuff-types?filters[name][$contains]=' + param
+    );
   }
 }
